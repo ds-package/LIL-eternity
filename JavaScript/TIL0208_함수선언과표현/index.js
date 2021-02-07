270a7addcbf7a1959a7e1e0e441f1b42794ec9c7
윤민희 <mini940511@gmail.com>
2021-02-08 02:33:01 +0900

    TIL0208_함수선언과표현
    - [01.function](##01.funtion)
    - [02.Parameters](##02.Parameters)
    - [03.Default Parameters](##03.DefaultParameters)
    - [04.Rest Parameters](##04.RestParameters)
    - [05.Local scope](##05.Localscope)
    - [06.Return](##06.Return)
    - [07.Early Return](##07.EarlyReturn)
    - [08.Function Expression](##08.FunctionExpression)
    - [09.Callback hell](##09.Callbackhell)
    - [10.Arrow Function](##10.ArrowFunction)
    - [11.IIFE](##11.IIFE)

--- JavaScript/TIL0208_함수선언과표현/index.js
+++ JavaScript/TIL0208_함수선언과표현/index.js
@@ -46,8 +46,117 @@ function showMessage(message, from = 'unknown') {
 
 showMessage('hi!');
 
-// Rest Parameters**
+// Rest Parameters
 
 function printAll(...args) {
+  for (let i = 0; i < args.length; i++) {
+    console.log(args[i]);
+  }
+
+  for (const arg of args) {
+    console.log(arg)
+  }
+
+  args.forEach((arg) => console.log(arg));
 
 }
+
+printAll('플랫폼', '디자이너', 'minhee');
+
+
+
+// Local scope
+
+let globalMessage = 'global';
+
+function printMsg() {
+  let message = '잠을자고싶어';
+  console.log(message);
+  console.log(globalMessage);
+
+  function printAnother() {
+    console.log(message);
+    let AnotherMessage = '출근은하기싫어';
+    console.log(AnotherMessage);
+  }
+  printAnother()
+}
+
+printMsg();
+
+
+// Return
+
+
+function sum(a, b) {
+  return a + b;
+}
+
+const result = sum(1, 2); //3
+
+console.log(`sum: ${sum(1, 2)}`);
+
+// Early Return
+
+function upgradeUser(user) {
+  if (user.point > 10) {
+    // long upgrade logic...
+  }
+}
+
+// Function Expression
+
+const print = function () {
+  console.log('print');
+};
+
+print();
+const printAgain = print;
+printAgain();
+const sumAgain = sum;
+console.log(sumAgain(1, 3));
+
+// Callback hell
+
+function randomQa(answer, printYes, printNo) {
+  if (answer === 'i love you') {
+    printYes();
+  } else {
+    printNo();
+  }
+}
+
+const printYes = function () {
+  console.log('Yes');
+};
+
+const printNo = function print() {
+  console.log('No');
+};
+
+randomQa('wrong', printYes, printNo);
+randomQa('i love you', printYes, printNo);
+
+// Arrow Function
+
+const simpleFc = function () {
+  console.log('simplePrint!');
+};
+simpleFc();
+
+const simpleFunction = () => console.log('moreSimplePrint!');
+simpleFunction();
+
+const add = (a, b) => a + b;
+
+const simpleMultiply = (a, b) => {
+  // do Something more 
+  return a * b;
+}
+
+
+//IIFE
+
+(function whatTheIife() {
+  console.log('IIEF');
+})();
